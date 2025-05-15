5822b220f8b8e2c4e2d47532b6533a82f250637c
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-15 16:41:15 +0330

    Create bot/messages package
    This commit creates the `bot/messages` package, which will
    contain pre-written messages for the bot to use. This will help
    to keep the bot's code clean and organized.

---
+++ bot/messages/__init__.py
@@ -0,0 +1,10 @@
+from bot.messages.common import WELCOME_MESSAGE, HELP_MESSAGE
+from bot.messages.tasks import TASK_HELP_MESSAGE
+from bot.messages.finance import FINANCE_HELP_MESSAGE
+
+__all__ = [
+    "WELCOME_MESSAGE", 
+    "HELP_MESSAGE",
+    "TASK_HELP_MESSAGE", 
+    "FINANCE_HELP_MESSAGE"
+]
